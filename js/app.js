1373b7da16e281845e2323257b9097c5e653d0d4
unknown <jim.darlow@gmail.com>
2018-06-26 22:54:34 -1000

    some of one done

--- js/app.js
+++ js/app.js
@@ -3,12 +3,50 @@
 
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
+/*==============good==============
+navmenu.addEventListener('click',showPrice);
 
+function showPrice(){
+    var getPrice=this.querySelectorAll('.inner')[0];
+    if(getPrice.style.display==='block'){
+        getPrice.style.display='none';
+    }else{
+        getPrice.style.display='block';
+    };
+ 
+ console.log(this.innerHTML);
+}
+======================good==========
+*/
+navmenu.addEventListener('click',showPrice);
 
-
-
+function showPrice(){
+    var getPrice=this.querySelectorAll('.inner')[0];
+    if(getPrice.style.display==='block'){
+        getPrice.style.display='none';
+    }else{
+        getPrice.style.display='block';
+    };
+ 
+ console.log(this.innerHTML);
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
+
+thumb.addEventListener('click',addOne);
+
+var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
+
+// function addOne(){
+//     var count=document.getElementsByClassName
+//     ('far fa-thumbs-down');
+//     count.innerHTML=(clicks);
+//     }
+
